93e3ee80d8e92e6a1ce16292aa566f3dd143105d
CrisArranz <c.arranz.arevalo@gmail.com>
2024-12-07 10:29:35 +0100

    chore: include dark mode

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/ui";
 import { Home, Bootstrap, Tailwind } from "./pages";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleDark = () => {
+    setIsDark(prev => !prev);
+  }
+
   return (
     <>
-      <Navbar options={['Inicio', 'TailwindCSS', 'Bootstrap']} className=""/>
+      <Navbar options={['Inicio', 'TailwindCSS', 'Bootstrap']} className="" onDarkMode={handleDark} isDark={isDark}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tailwindcss" element={<Tailwind />} />
